7f72097490e86687eaa45e7ff2738a9e4c86f860
rawaneltheni <rawaneltheni@gmail.com>
2025-11-17 23:01:04 +0200

    To be fixed by Ranazzz

---
+++ vue-netninja/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>{{title }}</h1>
+  	<!-- <input type="text" ref="temp">
+  	<button @click="handleClick()">click me</button> -->
+  	<div v-if="showModal">
+		<Modal :header="header" theme="sale" @close="toggleModal"/>
+  	</div>
+	<button @click="toggleModal()">Show</button>
+</template>
+
+<script>
+
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App', // root component  
+  components: {
+	Modal
+  },
+  data() {
+    return {
+      title: "My first Vue app",
+	    header: "Sign up for a Giveaway!",
+	   showModal: false,
+    }
+  },
+  methods: {
+    handleClick() {
+		console.log(this.$refs.name.classList.add('active'))
+		console.log(this.$refs.name.focus())
+		// classList.add('active') and .focus() are a plain javascript methdods we were able to use it using template refs 
+	},
+	toggleModal() {
+		this.showModal = !this.showModal
+	}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-netninja/src/Modal.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="backdrop" @click="closeModal()">
+        <div class="modal" :class="{sale: theme === 'sale'}">
+            <!-- using a prop -->
+            <h1>{{ header }}</h1>
+            <!-- hard coded without a prop -->
+            <p>modal content</p>
+        </div>
+    </div>
+</template>
+
+<!-- we can use <style scope> means the style will only be applied to this component -->
+<!-- better practice is to create a seprate globale style sheet -->
+<script>
+    export default {
+        props:  ['header', 'sale'],
+        methods: {
+            closeModal() {
+                this.$emit('close')
+            }
+        }
+    }
+</script>
+
+<style> 
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: whitesmoke;
+        border-radius: 10px;
+    }
+    .backdrop {
+        top: 0px;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .modal h1 {
+        color: aqua;
+        border: none;
+        padding: 0
+    }
+    .modal p {
+        font-style: normal;
+    }
+    .modal.sale{
+        background: crimson;
+        color: white
+    }
+    .model.sale h1 {
+        color: white;
+    }
+</style>
